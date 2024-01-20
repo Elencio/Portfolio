60e2da442fcfe5f6e4d3871178d1fd374c843e4d
Elencio <elenciocalado@gmail.com>
2024-01-20 11:34:54 -0300

    melhorando o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,37 +1,48 @@
 import { Link } from "react-router-dom";
-import { FaBehance, FaGithub, FaLinkedin } from "react-icons/fa6"
-import { CgMail } from "react-icons/cg"
+import { FaBehance, FaGithub, FaLinkedin } from "react-icons/fa6";
+import { CgMail } from "react-icons/cg";
 
 export default function Footer() {
     return (
-        <div className="nav-content mt-14">
-            <ul>
+        <div className="flex flex-row md:flex-row space-y-2 md:space-y-0 md:space-x-4 mt-14 justify-center">
+            <ul className="flex flex-row gap-4 justify-center items-center md:flex-row space-y-2 md:space-y-0 md:space-x-4">
                 <li>
-                    <Link to="https://github.com/Elencio" className="flex flex-row items-center gap-2 text-lg">
-                        Github
-                        <FaGithub color="#c1ff72" />
+                    <Link
+                        to="https://github.com/Elencio"
+                        className="flex flex-row items-center gap-2 text-lg"
+                    >
+                        <span className="hidden md:inline">Github</span>
+                        <FaGithub className="text-2xl md:text-lg" color="#c1ff72" />
                     </Link>
                 </li>
                 <li>
-                    <Link to="https://www.linkedin.com/in/elencio-calado-zivane/" className="flex flex-row items-center gap-2 text-lg">
-                        linkedin
-                        <FaLinkedin color="#c1ff72" />
+                    <Link
+                        to="https://www.linkedin.com/in/elencio-calado-zivane/"
+                        className="flex flex-row items-center gap-2 text-lg"
+                    >
+                        <span className="hidden md:inline">LinkedIn</span>
+                        <FaLinkedin color="#c1ff72" className="text-2xl md:text-lg" />
                     </Link>
                 </li>
                 <li>
-                    <Link to="/work-experience" className="flex flex-row items-center gap-2 text-lg">
-                        Gmail
-                        <CgMail color="#c1ff72" />
+                    <Link
+                        to="/work-experience"
+                        className="flex flex-row items-center gap-2 text-lg"
+                    >
+                        <span className="hidden md:inline">Gmail</span>
+                        <CgMail color="#c1ff72" className="text-2xl md:text-lg" />
                     </Link>
                 </li>
                 <li>
-                    <Link to="https://www.behance.net/elenciocalado" className="flex flex-row items-center gap-2 text-lg">
-                        Behance
-                        <FaBehance color="#c1ff72" />
+                    <Link
+                        to="https://www.behance.net/elenciocalado"
+                        className="flex flex-row items-center gap-2 text-lg"
+                    >
+                        <span className="hidden md:inline">Behance</span>
+                        <FaBehance color="#c1ff72" className="text-2xl md:text-lg" />
                     </Link>
                 </li>
             </ul>
         </div>
-    )
+    );
 }
-
